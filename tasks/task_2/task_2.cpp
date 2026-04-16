5b36fe005b2af5f6edab18a9ebe1fc400ca29fb2
Maksym Fomin <m94728963@gmail.com>
2026-04-16 21:21:38 +0300

    Add core libraries, console setup, and experimental data

---
+++ tasks/task_2/task_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    vector<double> X = { 20, 11, 10, 19, 22, 23, 11, 18, 14, 25 };
+
+    return 0;
+}
