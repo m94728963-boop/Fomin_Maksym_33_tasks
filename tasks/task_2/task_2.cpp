d1dca1891627c2feed33504f95979954c85e850a
Maksym Fomin <m94728963@gmail.com>
2026-04-16 21:22:11 +0300

    Implement mean calculation function

--- tasks/task_2/task_2.cpp
+++ tasks/task_2/task_2.cpp
@@ -5,11 +5,22 @@
 
 using namespace std;
 
+double calculateMean(const vector<double>& arr) {
+    double sum = 0;
+    for (double val : arr) {
+        sum += val;
+    }
+    return sum / arr.size();
+}
+
 int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
     vector<double> X = { 20, 11, 10, 19, 22, 23, 11, 18, 14, 25 };
+
+    double mean = calculateMean(X);
+    cout << "Середнє значення (x_cp): " << mean << endl;
 
     return 0;
 }
