a4d0c11fc6d7bc6bf42bd81b26de1f23c757c89e
Maksym Fomin <m94728963@gmail.com>
2026-04-16 21:23:03 +0300

    Implement deviation calculation and output results

--- tasks/task_2/task_2.cpp
+++ tasks/task_2/task_2.cpp
@@ -13,6 +13,15 @@ double calculateMean(const vector<double>& arr) {
     return sum / arr.size();
 }
 
+double calculateDeviation(const vector<double>& arr, double mean) {
+    double n = arr.size();
+    double sumSq = 0;
+    for (double val : arr) {
+        sumSq += (val * val);
+    }
+    return sqrt((1.0 / (n - 1.0)) * sumSq - (n / (n - 1.0)) * (mean * mean));
+}
+
 int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
@@ -20,7 +29,10 @@ int main() {
     vector<double> X = { 20, 11, 10, 19, 22, 23, 11, 18, 14, 25 };
 
     double mean = calculateMean(X);
+    double deviation = calculateDeviation(X, mean);
+
     cout << "Середнє значення (x_cp): " << mean << endl;
+    cout << "Середньоквадратичне відхилення (y): " << deviation << endl;
 
     return 0;
 }
