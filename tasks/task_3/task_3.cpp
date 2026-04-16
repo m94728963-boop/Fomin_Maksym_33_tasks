7543f22e598823265b46869930940d65b550d946
Maksym Fomin <m94728963@gmail.com>
2026-04-16 21:39:46 +0300

    Implement dynamic matrix creation, filling, and cleanup

---
+++ tasks/task_3/task_3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <iomanip>
+#include <cmath>
+#include <windows.h>
+
+using namespace std;
+
+int** createMatrix(int n) {
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    }
+    return matrix;
+}
+
+void deleteMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+void fillMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] = rand() % 87 - 43;
+        }
+    }
+}
+
+void printMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << setw(5) << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    srand(time(0));
+
+    int n;
+    cout << "Введіть розмір квадратної матриці n: ";
+    cin >> n;
+
+    if (n <= 0) return 0;
+
+    int** matrix = createMatrix(n);
+    fillMatrix(matrix, n);
+
+    cout << "\nПочаткова матриця:" << endl;
+    printMatrix(matrix, n);
+
+    deleteMatrix(matrix, n);
+    return 0;
+}
