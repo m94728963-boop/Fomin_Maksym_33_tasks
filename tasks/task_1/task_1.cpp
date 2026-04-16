597466a0f02dafe76f382e1775f20c8931fc03ec
Maksym Fomin <m94728963@gmail.com>
2026-04-16 17:57:42 +0300

    Add core libraries and array generation logic

--- tasks/task_1/task_1.cpp
+++ tasks/task_1/task_1.cpp
@@ -3,11 +3,24 @@
 #include <random>
 #include <chrono>
 #include <future>
-#include <algorithm>
-#include <windows.h>
+#include <windows.h> 
 
 using namespace std;
 using namespace std::chrono;
+
+void printArray(const vector<int>& arr);
+void generateArray(vector<int>& arr, int n);
+void bubbleSort(vector<int>& arr);
+void selectionSort(vector<int>& arr);
+int partition(vector<int>& arr, int low, int high);
+void quickSort(vector<int>& arr, int low, int high);
+int binarySearch(const vector<int>& arr, int key);
+void runTest(int n);
+
+void printArray(const vector<int>& arr) {
+    for (int x : arr) cout << x << " ";
+    cout << endl;
+}
 
 void generateArray(vector<int>& arr, int n) {
     random_device rd;
@@ -17,8 +30,3 @@ void generateArray(vector<int>& arr, int n) {
     for (int i = 0; i < n; ++i) arr.push_back(dis(gen));
 }
 
-void printArray(const vector<int>& arr) {
-    if (arr.size() > 60) return;
-    for (int x : arr) cout << x << " ";
-    cout << endl;
-}
